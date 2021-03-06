7dbe91a0f5bd9790dd2e593408f2f25f0638ff6c
Steven5678231 <53145087+Steven5678231@users.noreply.github.com>
2021-03-06 18:52:49 +1100

    Mar 5th

---
+++ jz39.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def IsBalanced_Solution(self, pRoot):
+        # write code here
+        return self.dfs(pRoot) != -1
+
+    def dfs(self, p):
+        return -1
